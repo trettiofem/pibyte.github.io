3842e7d7bb3de6c0a054a1f39ca025c1e704d05b
PiByte <jacob.2002.johansson@gmail.com>
2017-07-21 22:15:10 +0200

    Update poop.bat

--- poop.bat
+++ poop.bat
@@ -1,7 +1,10 @@
 @echo off
 color a
-title Poop
-echo hehe... pooooop
+title HAHA lol
+echo Turns out your computer has been infected by the teal worm!
 echo.
-timeout 5
+echo Show all your friends!!! LOL
+echo.
+echo ...okay, im sorry. run the "tealReset.vbs" file in "C:\tealTools" to clean the worm up! 
+pause
 exit
