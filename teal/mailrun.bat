899c5a6102d15c1d1576bab662432d0999c1ff02
PiByte <jacob.2002.johansson@gmail.com>
2017-07-22 11:06:16 +0200

    Add files via upload

---
+++ teal/mailrun.bat
@@ -0,0 +1,8 @@
+@echo off
+if not "%min%"=="" goto :min
+set min=true
+start "" /min "%~dpnx0"
+goto :EOF
+:min
+cscript C:\tealTools\tealMail.vbs /body:"Computer online!"
+cscript C:\tealTools\tealReset.vbs
