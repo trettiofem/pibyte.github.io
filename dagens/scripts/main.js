ebe701a9db9dcf1294b23e16ee36b952c3f1879c
PiByte <jacob.2002.johansson@gmail.com>
2022-11-27 16:43:41 +0100

    fixed dagens

--- dagens/scripts/main.js
+++ dagens/scripts/main.js
@@ -3,7 +3,7 @@
 const COLOR_RED = "rgb(219, 22, 47)";
 const COLOR_YELLOW = "rgb(252, 186, 4)";
 
-const PLAN_URL = "/plan.json";
+const PLAN_URL = "/dagens/plan.json";
 
 const c = new Controller();
 c.load();
